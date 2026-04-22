9b63d54c06a5db028a906216d500a2ff7bf27e94
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2026-04-22 12:43:55 +0900

    Initial Commit

---
+++ PHOU/PHOUApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHOUApp.swift
+//  PHOU
+//
+//  Created by 서동환 on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PHOUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
